233c8afbffaacaf02c653c7873a33aa66674bcf3
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-04-09 15:20:08 +0200

    Implement Deck logic and methods. Resolves #13

---
+++ app/src/main/java/com/doomhamsters/model/Deck.kt
@@ -0,0 +1,20 @@
+package com.doomhamsters.model
+
+class Deck {
+    // Arraylist instead of Dequeue cause you can insert easier into it.
+    private val cards = ArrayList<Card>()
+
+    fun draw(): Card? {
+        if (cards.isEmpty()) return null
+        return cards.removeAt(cards.size - 1)
+    }
+
+    fun shuffle() {
+        cards.shuffle() // built in kotlin shuffler
+    }
+
+    fun insertAt(card: Card, index: Int) {
+        val Index = index.coerceIn(0, cards.size)
+        cards.add(Index, card)
+    }
+}

f61d60e008a665be6068a7de24a563c8f2e7de3b
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-04-09 15:21:38 +0200

    Implement GameState snapshot. Resolves #12

---
+++ app/src/main/java/com/doomhamsters/model/GameState.kt
@@ -0,0 +1,18 @@
+package com.doomhamsters.logic
+
+import java.io.Serializable
+
+enum class Status {
+    Lobby,
+    Playing,
+    Finished
+}
+
+data class GameState(
+    val id: String,
+    val players: ArrayList<Player>,
+    val deck: Deck,
+    val discard: Discard,
+    var currentPlayerIndex: Int,
+    var status: Status
+) : Serializable // TODO: Figure out how to actually implement it into the backend.

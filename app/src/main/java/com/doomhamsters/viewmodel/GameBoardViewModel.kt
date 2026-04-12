631ab8dd02723bdc079a7561b712510491257688
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-04-12 15:44:18 +0200

    Implement GameBoardViewModel. Resolves #21

---
+++ app/src/main/java/com/doomhamsters/viewmodel/GameBoardViewModel.kt
@@ -0,0 +1,15 @@
+package com.doomhamsters.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.doomhamsters.logic.GameEngine
+import com.doomhamsters.logic.GameState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class GameBoardViewModel : ViewModel() {
+
+    private var gameEngine: GameEngine? = null
+
+    private val _gameState = MutableStateFlow<GameState?>(null)
+    val gameState: StateFlow<GameState?> = _gameState
+}

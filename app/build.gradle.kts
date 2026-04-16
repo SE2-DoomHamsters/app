eba0e43857f0d31271edda51690a6d0f7bf8f2e7
michelle-fe <michellefe@edu.aau.at>
2026-04-16 02:12:10 +0200

    adjust RulesScreen design and update README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,6 +123,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter.api)

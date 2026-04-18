4faf5a29bd22b4cb514be97d885055956ef3e057
SereinigChristian <christian.sereinig1@gmail.com>
2026-04-18 19:41:35 +0200

    alias(libs.plugins.kotlin.compose) wieder eingefügt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     id("jacoco")
     id("org.sonarqube") version "5.1.0.4882"
 }

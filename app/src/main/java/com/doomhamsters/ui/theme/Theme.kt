17864ab5f83d28473bad01102ad227740a02448c
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-05-11 19:11:53 +0200

    Added Colors to reuse in Theme.kt.

--- app/src/main/java/com/doomhamsters/ui/theme/Theme.kt
+++ app/src/main/java/com/doomhamsters/ui/theme/Theme.kt
@@ -9,35 +9,46 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
+val BackgroundCream = Color(0xFFEBEBEB)
+val CardDarkMaroon = Color(0xFF3B1525)
+val AccentOrange = Color(0xFFF08C50)
+val OutlineDark = Color(0xFF4A1A2A)
+val SnackStashColor = Color(0xFF86B049)
+val DoomColor = Color(0xFF9E2A2B)
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = AccentOrange,
+    secondary = CardDarkMaroon,
+    tertiary = OutlineDark,
+    background = OutlineDark,
+    surface = CardDarkMaroon,
+    onPrimary = CardDarkMaroon,
+    onSecondary = BackgroundCream,
+    onTertiary = BackgroundCream,
+    onBackground = BackgroundCream,
+    onSurface = BackgroundCream
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = AccentOrange,
+    secondary = CardDarkMaroon,
+    tertiary = OutlineDark,
+    background = BackgroundCream,
+    surface = BackgroundCream,
+    onPrimary = BackgroundCream,
+    onSecondary = BackgroundCream,
+    onTertiary = BackgroundCream,
+    onBackground = CardDarkMaroon,
+    onSurface = CardDarkMaroon
 )
 
 @Composable
 fun DoomHamstersTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {

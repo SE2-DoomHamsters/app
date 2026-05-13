fdb63ef900a447a30f7f3a96340a073c25fd790e
OlhaKupar <kupar@proagent.at>
2026-05-13 17:17:39 +0200

    fix: build fail fix, caused by "messy merge" via github in browser - removed stay characters, added missing LocalContext import

--- app/src/main/java/com/doomhamsters/LobbyScreens.kt
+++ app/src/main/java/com/doomhamsters/LobbyScreens.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -220,7 +221,6 @@ fun StartScreen(
 ) {
     val context = LocalContext.current
 
-    ) {
     Column(
         modifier = modifier
             .fillMaxSize()

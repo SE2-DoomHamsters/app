45d3a5c10a606df961b66331572594e45fcf4640
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-05-15 15:48:40 +0200

    Added Serialization to Card.kt. addition to #72

--- app/src/main/java/com/doomhamsters/model/Card.kt
+++ app/src/main/java/com/doomhamsters/model/Card.kt
@@ -1,9 +1,16 @@
 package com.doomhamsters.model
 
+import org.json.JSONObject
+
 enum class CardType {
     Doom,
     SnackStash,
     Normal
 }
 
-data class Card(val type: CardType)
+data class Card(val type: CardType) {
+    fun toJson(): JSONObject = JSONObject().put("type", type.name)
+    companion object {
+        fun fromJson(json: JSONObject) = Card(CardType.valueOf(json.getString("type")))
+    }
+}

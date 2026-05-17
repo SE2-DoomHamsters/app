4d2d2767ba0d28c38f5366afc47c89e9dba1f56b
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-05-17 16:34:46 +0200

    add PowerNap Definition #78 and #55

---
+++ app/src/main/java/com/doomhamsters/cards/definitions/PowerNapCardDefinition.kt
@@ -0,0 +1,31 @@
+package com.doomhamsters.cards.definitions
+
+
+import com.doomhamsters.cards.CardCommandId
+import com.doomhamsters.logic.cardcommands.CardCommand
+import com.doomhamsters.logic.cardcommands.CardCommandContext
+import com.doomhamsters.logic.cardcommands.CardCommandOutcome
+import com.doomhamsters.model.CardType
+
+object PowerNapCardDefinition : CardDefinition {
+    override val type: CardType = CardType.PowerNap
+    override val displayName: String = "Power Nap"
+    override val description: String = "Skip your draw phase and end your turn."
+    override val command: CardCommandDefinition = CardCommandDefinition(
+        id = CardCommandId.POWER_NAP,
+        endsTurn = true,
+        executor = PowerNapCommand
+    )
+
+    private object PowerNapCommand : CardCommand {
+        override val id: CardCommandId = CardCommandId.POWER_NAP
+
+        override fun execute(context: CardCommandContext): CardCommandOutcome {
+            context.engine.discardFromHand(context.player, context.card)
+            return CardCommandOutcome(
+                publicMessage = "${context.player.name} activated ${displayName} and skipped drawing.",
+                endsTurn = true
+            )
+        }
+    }
+}

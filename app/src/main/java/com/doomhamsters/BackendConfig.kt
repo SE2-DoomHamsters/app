31a22073b1106e21ad2de7cf00468ba6e03f9a44
michelle-fe <michellefe@edu.aau.at>
2026-05-20 11:02:00 +0200

    test: add tests for leave lobby

--- app/src/main/java/com/doomhamsters/BackendConfig.kt
+++ app/src/main/java/com/doomhamsters/BackendConfig.kt
@@ -2,5 +2,5 @@ package com.doomhamsters
 
 /** Provides the backend host configuration used by the app. */
 object BackendConfig {
-    const val BASE_URL = "10.0.2.2:53217"
+    const val BASE_URL = "10.0.2.2:8080"
 }

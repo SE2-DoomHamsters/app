9d9998a02290aa335f5117100aa550b33986ac77
SereinigMichael <michael.sereinig1@gmail.com>
2026-06-10 16:43:51 +0200

    Registriert die 4-Hamster-Karte im Frontend

--- app/src/main/java/com/doomhamsters/cards/CardRegistry.kt
+++ app/src/main/java/com/doomhamsters/cards/CardRegistry.kt
@@ -5,6 +5,7 @@ package com.doomhamsters.cards
 import com.doomhamsters.cards.definitions.CardCommandDefinition
 import com.doomhamsters.cards.definitions.CardDefinition
 import com.doomhamsters.cards.definitions.DoomCardDefinition
+import com.doomhamsters.cards.definitions.FourHamstersCardDefinition
 import com.doomhamsters.cards.definitions.NormalCardDefinition
 import com.doomhamsters.cards.definitions.PowerNapCardDefinition
 import com.doomhamsters.cards.definitions.QuickPeekCardDefinition
@@ -19,6 +20,7 @@ object CardRegistry {
         SnackStashCardDefinition,
         PowerNapCardDefinition,
         QuickPeekCardDefinition,
+        FourHamstersCardDefinition,
         NormalCardDefinition
     )
 

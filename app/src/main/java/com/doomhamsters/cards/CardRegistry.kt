52624ff320471c9caeba2d9a1b5e9b229bfa424f
ruzicka <ruzicka365@gmail.com>
2026-06-10 18:22:44 +0200

    small fix

--- app/src/main/java/com/doomhamsters/cards/CardRegistry.kt
+++ app/src/main/java/com/doomhamsters/cards/CardRegistry.kt
@@ -9,6 +9,7 @@ import com.doomhamsters.cards.definitions.NormalCardDefinition
 import com.doomhamsters.cards.definitions.PowerNapCardDefinition
 import com.doomhamsters.cards.definitions.QuickPeekCardDefinition
 import com.doomhamsters.cards.definitions.SnackStashCardDefinition
+import com.doomhamsters.cards.definitions.BegForSnacksCardDefinition
 import com.doomhamsters.model.Card
 import com.doomhamsters.model.CardType
 

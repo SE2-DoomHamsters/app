7356e6c0fa6fbe8ee98c5c9a7dabd33b39168507
SereinigMichael <michael.sereinig1@gmail.com>
2026-06-10 16:42:41 +0200

    Erweitert die Enums um den Typ und die ID der neuen Hamster-Karte

--- app/src/main/java/com/doomhamsters/cards/CardCommandId.kt
+++ app/src/main/java/com/doomhamsters/cards/CardCommandId.kt
@@ -4,6 +4,7 @@ package com.doomhamsters.cards
 /** Lists the supported activatable card command identifiers. */
 enum class CardCommandId {
     POWER_NAP,
+    FOUR_HAMSTERS,
     QUICK_PEEK;
 
     companion object {

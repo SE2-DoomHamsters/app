bb5e3097d73afc7749b5e135d9159d145bada4ad
ruzicka <ruzicka365@gmail.com>
2026-06-13 18:35:56 +0200

    added things to the cardfaces

--- app/src/main/java/com/doomhamsters/ui/gameboard/CardFaces.kt
+++ app/src/main/java/com/doomhamsters/ui/gameboard/CardFaces.kt
@@ -60,6 +60,11 @@ fun CardFaceUp(card: Card, isSelected: Boolean, isDefocused: Boolean, modifier: 
         CardType.SnackStash -> Triple(SnackStashColor, Color(0xFFA3C968), CardDarkMaroon)
         CardType.PowerNap -> Triple(Color(0xFFB8D8E8), Color(0xFF6A9FB5), CardDarkMaroon)
         CardType.QuickPeek -> Triple(Color(0xFFF8E7A2), Color(0xFFD6A93A), CardDarkMaroon)
+        CardType.HamsterFat,
+        CardType.HamsterNinja,
+        CardType.HamsterSleepy,
+        CardType.HamsterGremlin,
+        CardType.HamsterZombi -> Triple(Color(0xFFD4A8D0), Color(0xFF9B6B96), CardDarkMaroon)
         CardType.Normal -> Triple(BackgroundCream, AccentOrange, CardDarkMaroon)
     }
 

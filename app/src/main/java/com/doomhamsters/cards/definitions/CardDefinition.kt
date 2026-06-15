f39e47e6c5ce68126e2a3d6f48165d15b88e506b
SereinigMichael <michael.sereinig1@gmail.com>
2026-06-15 11:41:12 +0200

    Erstellen der CardCommandDefinition für Two-Hamsters Combo #125

--- app/src/main/java/com/doomhamsters/cards/definitions/CardDefinition.kt
+++ app/src/main/java/com/doomhamsters/cards/definitions/CardDefinition.kt
@@ -18,5 +18,8 @@ data class CardCommandDefinition(
     val actionPath: String = "card/activate",
     val endsTurn: Boolean = false,
     val privateResult: Boolean = false,
+    val requiresTargetPlayer: Boolean = false,
+    val requiresCardType: Boolean = false,
+    val requiresHamsterType: Boolean = false,
     val executor: CardCommand? = null
 )

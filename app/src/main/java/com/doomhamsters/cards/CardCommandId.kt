04958cbac6edf1700e663f4c257f26c6e477c221
TheRealSlyzzz <alexanderkarner16@gmail.com>
2026-06-16 09:14:03 +0200

    added SIGN_OF_FATE to CardCommandId

--- app/src/main/java/com/doomhamsters/cards/CardCommandId.kt
+++ app/src/main/java/com/doomhamsters/cards/CardCommandId.kt
@@ -4,6 +4,7 @@ package com.doomhamsters.cards
 /** Lists the supported activatable card command identifiers. */
 enum class CardCommandId {
     POWER_NAP,
+    SIGN_OF_FATE,
     QUICK_PEEK;
 
     companion object {
